ed29987dd6e9784c32439884bf25d7599040b20d
Hea092024 <Hea092024@gmail.com>
2024-11-14 18:11:43 +0100

    .

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let name = "Hector";
-let age = 41;
-let person = {
-    name: "Hector", 
-    age: 41
-};
-// Dot Notation
-person.name = "Lise";
+// let name = "Hector";
+// let age = 41;
+// let person = {
+//     name: "Hector", 
+//     age: 41
+// };
+// // Dot Notation
+
+// person.name = "Lise";
 
 
-console.log(person.name);
+// console.log(person.name);
 
-//Bracket Notation
-let selection = "name";
-person['selection'] = "Madelen";
+// //Bracket Notation
+
+// let selection = "name";
+// person['selection'] = "Madelen";
+
+// let selectedColors = ["red","blue"];
+// selectedColors[2]=1;
+// console.log(selectedColors.length);
+
+function greet(name){
+    console.log("Hello " + name);
+}
+
+greet("john");
