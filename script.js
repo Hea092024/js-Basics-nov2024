f261cd3cd62cc43656c33e6f8c1fbda675c02c48
Hea092024 <Hea092024@gmail.com>
2024-11-14 18:02:22 +0100

    .

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ let person = {
     name: "Hector", 
     age: 41
 };
+// Dot Notation
+person.name = "Lise";
 
-console.log(person);
+
+console.log(person.name);
+
+//Bracket Notation
+let selection = "name";
+person['selection'] = "Madelen";
